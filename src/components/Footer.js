ffe5609d76adf250b680794c7b113f91ffda36ae
Kate Steed <sunfiresteed@yahoo.com>
2023-03-25 13:53:18 -0600

    started adding footer

---
+++ src/components/Footer.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <ul>
+        <li>
+          <a
+            href="https://github.com/Ksteed8859"
+          >
+            GitHub
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://www.linkedin.com/in/katelynsteed885926/"
+          >
+            LinkedIn
+          </a>
+        </li>
+        <li className="nav-item">
+          <a
+            href="https://www.instagram.com/katelyn_steed/"
+          >
+            Instagram
+          </a>
+        </li>
+    </ul>
+    )
+}
+
+export default Footer;
