da187eca83924f1f08da6e0a1c49750f43d2a78c
Kate Steed <sunfiresteed@yahoo.com>
2023-03-26 16:20:14 -0600

    redid file structure, added footer and home page

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function Footer() {
+  return (
+    <footer>
+        <h3>Connect with Me!</h3>
+        <button><a href="https://github.com/Ksteed8859">Github</a></button>
+        <button><a href="https://www.linkedin.com/in/katelynsteed885926/"></a>LinkedIn</button>
+        <button><a href="https://www.facebook.com/katelyn.steed.1/"></a>Facebook</button>
+
+    </footer>
+  );
+}
--- src/components/PortfolioContainer.js
+++ src/components/PortfolioContainer.js
@@ -1,18 +1,17 @@
 import React, { useState } from 'react';
-import NavTabs from './NavTabs';
-import About from './pages/About';
+import Navbar from './Navbar';
+import Footer from './Footer';
+import Home from './pages/Home';
 import Portfolio from './pages/Portfolio';
 import Resume from './pages/Resume';
 import Contact from './pages/Contact';
 
-
 export default function PortfolioContainer() {
-  const [currentPage, setCurrentPage] = useState('About');
+  const [currentPage, setCurrentPage] = useState('Home');
 
-  // This method is checking to see what the value of `currentPage` is. Depending on the value of currentPage, we return the corresponding component to render.
   const renderPage = () => {
-    if (currentPage === 'About') {
-      return <About />;
+    if (currentPage === 'Home') {
+      return <Home />;
     }
     if (currentPage === 'Portfolio') {
       return <Portfolio />;
@@ -27,10 +26,9 @@ export default function PortfolioContainer() {
 
   return (
     <div>
-      {/* We are passing the currentPage from state and the function to update it */}
-      <NavTabs currentPage={currentPage} handlePageChange={handlePageChange} />
-      {/* Here we are calling the renderPage method which will return a component  */}
+      <Navbar currentPage={currentPage} handlePageChange={handlePageChange} />
       {renderPage()}
+      <Footer/>
     </div>
   );
 }
