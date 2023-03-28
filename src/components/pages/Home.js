34fa3d32f6a8dec2315a30abb160c9705e70fdb1
Kate Steed <sunfiresteed@yahoo.com>
2023-03-28 15:07:11 -0600

    added css and finished about page

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function Home() {
   return (
-    <section className="homeimage" id="home">
+    <section className="Home">
         <div className="container-fluid">
             <div className="row">
             <div className="col-lg-12 col-md-12 col-sm-12 col-xs-12 text">
